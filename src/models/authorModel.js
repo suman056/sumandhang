da46f7367da961c7b47885f740a9a0bdc255135a
imtiyazz24 <imtiyaz.ansari24@gmail.com>
2022-06-21 15:20:22 +0530

    y

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,7 +1,41 @@
 const mongoose = require('mongoose');
+var validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
-const authorSchema = new mongoose.Schema( {
-   
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    title: {
+        type: String,
+        enum: ["Mr", "Mrs", "Miss"],
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email address is required',
+        unique: true,
+        'valid email': [validateEmail, 'Please fill a valid email address'],
+        trim: true
+        // match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password:{
+        type:String,
+        required:true,
+        trim: true
+    }
+
+
 
 }, { timestamps: true });
 
