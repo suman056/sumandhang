cf041c4f56fe1f2c6f67c929be1c3382e4936553
Souvik Jha <jhasouvik4@gmail.com>
2022-06-21 15:41:59 +0530

    created api

--- src/controllers/blogdcontroller.js
+++ src/controllers/blogdcontroller.js
@@ -2,12 +2,52 @@ const authorModel= require("../models/authorModel")
 const blogModel=require("../models/blogModel")
 
  const createAuthor= async function(req,res){
-   
- }
+   try{
+      let data = req.body
+      if(!data.fname)
+      return res.status(400).send("fname is mandatory")
+      if(!data.lname)
+      return res.status(400).send("lname is mandatory")
+      if(!data.title)
+      return res.status(400).send("title is mandatory")
+      if(!data.email)
+      return res.status(400).send("email is mandatory")
+      if(!data.password)
+      return res.status(400).send("email is mandatory")
 
- const createBlog=async function(req,res){
-   
+
+      let saveData = await authorModel.create(data)
+      res.status(201).send({msg: saveData})
+   }catch(err){
+      console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  }
+}
+
+ 
+
+
+const createBlog=async function(req,res){
+   try{
+      let data = req.body
+      if(!data.title)
+      return res.status(400).send("title is mandatory")
+      if(!data.body)
+      return res.status(400).send("body is mandatory")
+      if(!data.authorId)
+      return res.status(400).send("authorId is mandatory")
+      if(!data.category)
+      return res.status(400).send("category is mandatory")
+
+      let saveData = await authorModel.create(data)
+      res.status(201).send({msg: saveData})
+   }catch(err){
+      console.log("This is the error :", err.message)
+      res.status(500).send({ msg: "Error", error: err.message })
  }
+}
+
+
 
  const getBlog=async function(req,res){
    
