fdb1d25835611f3aa28f3a7d4f64322aea706ef0
jagcho <jagadishchodry@gmail.com>
2022-07-06 13:07:25 +0530

    books model

---
+++ src/models/booksModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+
+    title: { type: String, required: true, unique: true, trim: true },
+    excerpt: { type: String, required: true, trim: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user' },
+    ISBN: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    subcategory: { type: [String], required: true },
+    reviews: { type: Number, default: 0},
+    isDeleted: { type: Boolean, default: false }
+
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('book', bookSchema);
