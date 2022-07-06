551d2b17e164c8c5308969b46a553e5f8ce56a1f
jagcho <jagadishchodry@gmail.com>
2022-07-06 12:17:09 +0530

    cluster attached

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const route=require("./routes/route")
 
 app.use(bodyParser.json())
 
-mongoose.connect("",{useNewUrlParser:true})
+mongoose.connect("mongodb+srv://Jagcho:71nEXJtXcYfVx8T6@cluster0.5bg4mzz.mongodb.net/group25Database",{useNewUrlParser:true})
 .then(()=>console.log("MongoDb is connected"))
 .catch(err=>console.log(err.message))
 
