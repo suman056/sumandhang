c77b218efd467eed8cfcbff00e4ef671dabaa75e
jagcho <jagadishchodry@gmail.com>
2022-07-06 13:29:33 +0530

    updated book model

--- src/models/booksModel.js
+++ src/models/booksModel.js
@@ -1,17 +1,51 @@
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 const bookSchema = new mongoose.Schema({
 
-    title: { type: String, required: true, unique: true, trim: true },
-    excerpt: { type: String, required: true, trim: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user' },
-    ISBN: { type: String, required: true, unique: true },
-    category: { type: String, required: true },
-    subcategory: { type: [String], required: true },
-    reviews: { type: Number, default: 0},
-    isDeleted: { type: Boolean, default: false },
-    releasedAt: {type:Date, required:true},
-    deletedAt: {type:Date}
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    excerpt: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: [String],
+        required: true
+    },
+    reviews: {
+        type: Number,
+        default: 0
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+        default: moment().format("YYYY-MM-DD"),
+        required: true,
+    },
+    deletedAt: { type: Date }
 
 },
     { timestamps: true }
